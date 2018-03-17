3f72fc3bf5c9253861ff2936ffb9e2251ccb4a2b
nmschorr <nancyschorr@yahoo.com>
2018-03-17 15:40:27 -0700

    Cleaned up final strings into constructor.
    Signed-off-by: nmschorr <nancyschorr@yahoo.com>

--- ReadJSON/src/main/java/com/nmschorr/ReadJSON.java
+++ ReadJSON/src/main/java/com/nmschorr/ReadJSON.java
@@ -13,56 +13,99 @@ import java.io.FileNotFoundException;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.net.URL;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Iterator;
+import java.util.Collection;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONArray;
 
 public class ReadJSON {
-	final static String myJsonFileName = "C:\\jsondata2.json";
-	final static String locUrlString = "http://jsonplaceholder.typicode.com/albums";
+	final String myJsonFileName, locUrlString;
 
-	public static void main(String[] args) throws Exception {
+	public ReadJSON() {
 		System.setProperty("http.agent", "Chrome");
+		myJsonFileName =  "C:\\jsondata2.json";
+		locUrlString = "http://jsonplaceholder.typicode.com/albums";
 		System.out.println("Beginning...");
-		//readUrlData();  // reads json data from a url http page
-		try {    	readJson(ReadJSON.readScanFile(myJsonFileName));  } catch(  Exception e) { e.printStackTrace(); }
-		System.out.println("\nProgram Finished");
 	}
 
+	public static void main(String[] args) throws Exception {
+		ReadJSON myThis = new ReadJSON();     // constructor, set up strings
+		//readUrlData();  // reads json data from a url http page
+		try {    	
+			String FileAsString =  readScanFile(myThis.myJsonFileName); 
+
+			JSONArray filecontentsAsJSONArrayMain = makeJsonArrayReallyHashmapsFrFileStringContent(FileAsString); 
+			// turn array into objects, hashmap set
+
+
+		} catch(  Exception e) { e.printStackTrace(); }
+		System.out.println("\nProgram Finished");
+	}
 
 	// This method plays with converting JSON objects: use debugger to see them List<Object> myArrayList = new ArrayList<Object>();
 	// HashMap<String, String> map = new HashMap<String, String>();  // shows casting of Object to HashMap Map<String,String> mapsObj = new HashMap<String,String>();	
 
-	static void readJson(String long1string) throws Exception {
-		JSONArray myJsonArry = new JSONArray(long1string); 
-		List<Object> mArrayofObjects = new ArrayList<Object>(myReadJsonArrayToObList(myJsonArry));
+	static  JSONArray makeJsonArrayReallyHashmapsFrFileStringContent(String fileContents) throws Exception {
+		JSONArray filecontentsAsJSONArray = new JSONArray(fileContents); 
+		System.out.println("\n inside makeJsonArrayFromFileStringContent. returning: " +filecontentsAsJSONArray.toString() );
+	    return filecontentsAsJSONArray;
+	}	    
+
+	static void chgToHashMapSET(ArrayList<Object> locArryList ) throws Exception {
 		Collection<Map<String,String>> myMapsHashSet = new HashSet<Map<String,String>>();	
-		
-		final JSONArray json2arr = new JSONArray(long1string); 
-	    List<Object> arryOf4Objects= new ArrayList<Object>();
-	    arryOf4Objects=myReadJsonArrayToObList(json2arr);
-		for (int i=0; i < arryOf4Objects.size(); i++) {
-			Object myLocObject = arryOf4Objects.get(i);
-			HashMap<String, String> locHashMap = ((HashMap<String, String>)myLocObject);
-			myMapsHashSet.add(locHashMap);
+		for (int i=0; i < locArryList.size(); i++) {
+			Object myLocObject = locArryList.get(i);
+			HashMap<String,Object> hm = chgToHashMap(myLocObject);
+			HashMap<Object,Object> hm2;
+			//myMapsHashSet.add((HashMap<Object,Object>)hm);
 		}
-
-
-		for ( int arrLocCtr = 0; arrLocCtr < json2arr.length(); arrLocCtr++) { 
-		//	 JSONObject myJsonObj = json2arr.optJSONObject(arrLocCtr);
-			 Iterator<Object> myJsonItr = json2arr.iterator();
-
-			while(myJsonItr.hasNext()) {
-			    Object myObj = myJsonItr.next();
-				System.out.println("myJsonArry element obj" + myObj + " \n\n");
-
-				for(int i=0; i<json2arr.length(); i++){
-					json2arr.getJSONObject(i);
-				} //for
-			} //while
-		}  //for
+		System.out.println("\n inside chgToHashMapSET. returning: " + "nothing" );
 	}
+
+	static HashMap chgToHashMap(Object locObj ) throws Exception {
+		HashMap<String, String> locHashMap= ((HashMap<String, String>)locObj);
+		return locHashMap;
+	}
+
+
+
+void fetchOneJsonToedit(ArrayList<JSONObject> json2arr ) {
+     for ( int arrLocCtr = 0; arrLocCtr < json2arr.size()    ; arrLocCtr++) { 
+	         JSONObject myJsonObj = json2arr.get(arrLocCtr) ;      //     .optJSONObject(arrLocCtr);
+	 		 Iterator<JSONObject> myJsonItr = json2arr.iterator();
+     
+	 while(myJsonItr.hasNext()) {
+	      Object myObj = myJsonItr.next();
+	      System.out.println("myJsonArry element obj" + myObj + " \n\n");
+	 }
+     }
+     
+     for(int i=0; i<json2arr.size(); i++){
+           json2arr.get(i) ;         
+     }
+     }
+
+	//void newFunction() {
+	//		for ( int arrLocCtr = 0; arrLocCtr < json2arr.length(); arrLocCtr++) { 
+	//		//	 JSONObject myJsonObj = json2arr.optJSONObject(arrLocCtr);
+	//			 Iterator<Object> myJsonItr = json2arr.iterator();
+	//
+	//			while(myJsonItr.hasNext()) {
+	//			    Object myObj = myJsonItr.next();
+	//				System.out.println("myJsonArry element obj" + myObj + " \n\n");
+	//
+	//				for(int i=0; i<json2arr.length(); i++){
+	//					json2arr.getJSONObject(i);
+	//				}}}
+	//}
+
 
 	// for this and toList below: thanks to: http://stackoverflow.com/questions/21720759/convert-a-json-string-to-a-hashmap
 
@@ -73,59 +116,55 @@ public class ReadJSON {
 		while (keysItr.hasNext()) {
 			String myKey = keysItr.next();
 			Object myVal = locPassInObj.get( keysItr.next() );
-
-			if (myVal instanceof JSONArray) {
-				myVal = myReadJsonArrayToObList((JSONArray) myVal);
-			}
-			else if (myVal instanceof JSONObject) {
-				myVal = myJsonObjToMap((JSONObject) myVal);
-			}
+			myVal = myJsonObjToMap((JSONObject) myVal);
 			myMap.put(myKey, myVal);
 		}
 		return myMap;
 	}
 
- 
+
 	public static List<Object> myReadJsonArrayToObList(JSONArray passedInJsonArray) throws JSONException {
+		Object locObject2= null;
 		List<Object> myObjectList = new ArrayList<Object>();
 		for(int i = 0; i < passedInJsonArray.length(); i++) {
-			Object locObject= passedInJsonArray.get(i);
-			if (locObject instanceof JSONArray) {
-				locObject = myReadJsonArrayToObList((JSONArray) locObject);
-			} 	
-			myObjectList.add(locObject);
-		} 	return myObjectList;
+			Object locJson= passedInJsonArray.get(i);
+			locObject2 = myReadJsonArrayToObList((JSONArray) locJson);
+			myObjectList.add(locObject2);
+		} 	
+		return myObjectList;
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
+
+
+
+
 	public static String readScanFile(String filename) throws FileNotFoundException {
-	    String myDelim = "\\Z";
+		out.println ("in readScanFile");
+		String myDelim = "\\Z";
 		String locString = new String("");  // new empty string
 		File myFile = new File(filename);
 		Scanner myScanner = new Scanner(myFile);
 		locString = myScanner.useDelimiter(myDelim).next();
 		myScanner.close();			
+		out.println ("in readScanFile. returning string: " + locString);
 		return locString;
 	}  //myScanner()
 
 	void readUrlData2() throws Exception {
 		String builderLine;
 		StringBuilder myStrBuilder = new StringBuilder();
-	    Reader bufReader = new BufferedReader(
-		    new InputStreamReader(new URL(locUrlString).openStream()));	
-		    while((builderLine = ((BufferedReader)bufReader).readLine()) != null) {
-			    myStrBuilder.append(builderLine);
+		Reader bufReader = new BufferedReader(
+				new InputStreamReader(new URL(locUrlString).openStream()));	
+		while((builderLine = ((BufferedReader)bufReader).readLine()) != null) {
+			myStrBuilder.append(builderLine);
 		}}
 
-	}	//class
+}	//class
 
 
 
